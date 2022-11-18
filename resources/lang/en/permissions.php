3b0ce0929c7ef3161490944d4f1620ab7c7eb598
Maciej Rymarz <maciej.rymarz@escolasoft.com>
2022-11-18 16:25:20 +0100

    Add translations

---
+++ resources/lang/en/permissions.php
@@ -0,0 +1,7 @@
+<?php
+
+use EscolaLms\Video\Enums\VideoPermissionEnum;
+
+return [
+    VideoPermissionEnum::VIDEO_PROCESS_STATES_LIST => 'Video process states list',
+];
--- src/EscolaLmsVideoServiceProvider.php
+++ src/EscolaLmsVideoServiceProvider.php
@@ -37,6 +37,7 @@ class EscolaLmsVideoServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/routes.php');
+        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'video');
 
         if ($this->app->runningInConsole()) {
             $this->bootForConsole();
